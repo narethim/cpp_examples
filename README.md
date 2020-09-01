c2d2c7d52c5b800b9b4bb64a1350df4d43e39341
Nareth Im <narethi@yahoo.com>
2020-09-01 13:01:53 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# cpp_examples
+# cpp_examples - Cpp Example code and Unit Test using Google Test
+
+## Prerequisite
+
+* Java SDK Development
+* Apache ant
+* Google Test
+
+## Directory structure
+
+```sh
+
+cpp_example
+  |
+  + src ( cpp files and Makefile )
+  |
+  + test
+    |
+    + src ( Unit Test files and Makefile )
+    
+```
+
+## How to run it
+
+```sh
+
+cd app_examples
+
+ant 
+
+ant run
+
+ant clean
+
+ant run-test
+
+```
+
---
+++ test/src/Multiply_Test.cpp
@@ -0,0 +1,30 @@
+#include <limits.h>
+#include "gtest/gtest.h"
+#include "Multiply.h"
+
+class MultiplyTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+  }
+
+  virtual void TearDown() {
+  }
+};
+
+TEST_F(MultiplyTest,twoValues){
+    const int x = 4;
+    const int y = 5;
+    Multiply multiply;
+    EXPECT_EQ(20,multiply.twoValues(x,y));
+    EXPECT_EQ(6,multiply.twoValues(2,3));
+}
+
+TEST_F(MultiplyTest,twoValues2){
+    const int x = 5;
+    const int y = 6;
+    Multiply multiply;
+    EXPECT_EQ(30,multiply.twoValues(x,y));
+    EXPECT_EQ(15,multiply.twoValues(5,3));
+    EXPECT_EQ(18,multiply.twoValues(6,3));
+}
+
